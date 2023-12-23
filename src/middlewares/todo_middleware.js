96af16b2dcf436977aff6f693683b0c6d056b2f7
Vishnu Kumar <vermaji1891@gmail.com>
2023-12-23 14:14:44 +0530

    added badRequest class

--- src/middlewares/todo_middleware.js
+++ src/middlewares/todo_middleware.js
@@ -1,8 +1,9 @@
+const BadRequest = require("../error/bad_request")
 const errorResponse = require("../utils/error_response")
 
 function createTodoValidator (request, response, next) {
     if(!request.body.title) {
-        return response.json(errorResponse("title is missing", {message: "title missing form req body"}))
+        return response.status(404).json(errorResponse("Title is not present in the incoming request", new BadRequest("Title")))
     }
     // if everhting goes successfull then call next
     next()
